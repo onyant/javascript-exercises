2489335fd41cd33bb369ae6694d4f48ea91bd123
onyant <onya34100@gmail.com>
2023-10-11 20:59:18 +0200

    1-2-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let result=""
+    const length=str.length
+    for(let i=length-1;i>=0; i--){
+        result+=str.charAt(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(list,...numbers) {
+    let newlist=[];
+    
+    list.forEach(element => {
+        if(!list.includes(element)){
+            newlist.push(element)
+        }
+    }
+    )
 
-};
+    return newlist;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
